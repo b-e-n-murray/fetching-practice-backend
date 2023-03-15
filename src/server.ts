f0db335beb38b57225030261a5b7e9ea8f4b1ba2
Ben Murray <benmurray573@gmail.com>
2023-03-15 13:04:22 +0000

    added pg

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { Client } from "pg";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 dotenv.config();
+
 const PORT_NUMBER = process.env.PORT ?? 4000;
 
+const client = new Client(process.env.DATABASE_URL);
+client.connect();
 
 
 app.listen(PORT_NUMBER, () => {
